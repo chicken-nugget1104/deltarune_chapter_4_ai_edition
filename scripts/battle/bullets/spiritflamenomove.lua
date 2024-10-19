3db09160ecdcc242f5e8e0ec348f6d5fd65cded7
jmxamongusmodder <160080970+chicken-nugget1104@users.noreply.github.com>
2024-10-18 22:25:59 -0400

    Update spiritflamenomove.lua

--- scripts/battle/bullets/spiritflamenomove.lua
+++ scripts/battle/bullets/spiritflamenomove.lua
@@ -4,9 +4,9 @@ function SpiritFlame:init(x, y)
     -- Sprite path for spirit flame
     super.init(self, x, y, "bullets/flame")
 
-    self:setHitbox(0, 0, self.width, self.height)
     self.destroy_on_hit = true
-    self:setScale(1, 1)
+    self:setScale(0.99, 0.99)
+    self:setHitbox(0, 0, self.width, self.height)
     self.physics.speed = 2
 end
 
