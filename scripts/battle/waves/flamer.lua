af31008d2efd01d83d9ad7d99dbe30e4d300a17f
TFLTV <105890618+TFLTV@users.noreply.github.com>
2024-10-18 23:36:08 -0500

    Update flamer.lua

--- scripts/battle/waves/flamer.lua
+++ scripts/battle/waves/flamer.lua
@@ -4,16 +4,28 @@ function SpiritFlamesWave:onStart()
     -- Every 1 second, spawn a flame that moves toward the SOUL
     self.timer:every(1, function()
         local arena = Game.battle.arena
-        local x = Utils.random(arena.left + 50, arena.right - 50)
-        local y = Utils.random(arena.top + 50, arena.bottom - 50)
+        local axis = Utils.pick({"hori","vert"}) -- horizontal, vertical
+        local side = Utils.pick({"left","right"})
+
+        local x = axis == "hori" and 
+        (side == "left" and Utils.random(arena:getLeft() - 50, arena:getLeft() - 10) or Utils.random(arena:getRight() + 10, arena:getRight() + 50)) or 
+        Utils.random(arena:getLeft(), arena:getRight())
+
+        local y = axis == "vert" and
+        (side == "left" and Utils.random(arena:getTop() - 50, arena:getTop() - 10) or Utils.random(arena:getBottom() + 10, arena:getBottom() + 50)) or
+        Utils.random(arena:getTop(), arena:getBottom())
+
 
         -- Spawn Spirit Flame bullet at random positions
         local flame = self:spawnBullet("spiritflame", x, y)
-        flame.physics.speed = Utils.random(2, 4) -- Set a slow speed
+        flame.physics.speed = Utils.random(2, 3) -- Set a slow speed
     end)
 end
 
 function SpiritFlamesWave:update()
+    for i, bullet in ipairs(self.bullets) do
+        bullet.physics.direction = Utils.angle(bullet.x, bullet.y, Game.battle.soul.x, Game.battle.soul.y)
+    end
     super.update(self)
 end
 
