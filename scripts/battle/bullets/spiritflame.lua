5186ca045b0188a7dd792dc6e8936dedf23e3ad2
jmxamongusmodder <160080970+chicken-nugget1104@users.noreply.github.com>
2024-10-19 09:38:32 -0400

    Revert "manual change"
    This reverts commit b0f776a48bc25ff06c9c48e4c068fa862834eeb8.

--- scripts/battle/bullets/spiritflame.lua
+++ scripts/battle/bullets/spiritflame.lua
@@ -3,7 +3,6 @@ local SpiritFlame, super = Class(Bullet)
 function SpiritFlame:init(x, y)
     super.init(self, x, y, "bullets/flame") -- only texture change...
     self:setScale(1)
-    self.destroy_on_hit = true
 end
 
 return SpiritFlame
