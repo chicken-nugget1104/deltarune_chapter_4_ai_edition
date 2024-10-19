29d1db9436189000b5d0f603ac22471469bb5ac2
TFLTV <105890618+TFLTV@users.noreply.github.com>
2024-10-19 13:06:45 -0500

    Update yeet.lua

--- scripts/battle/bullets/yeet.lua
+++ scripts/battle/bullets/yeet.lua
@@ -1,9 +1,15 @@
 local YeetBullet, super = Class(Bullet)
 
 function YeetBullet:init(x, y, dir, speed)
-    -- Last argument = sprite path
-    super.init(self, x, y, "bullets/yeetbullet" .. Utils.random(1,3,1))
-
+    self.yeet_id = Utils.random(1,3,1)
+    super.init(self, x, y, "bullets/yeetbullet"..yeet_id)
+    local hitboxes = {
+        {0,0,10,10} -- hitbox for yeetbullet1
+        {0,0,20,20} -- hitbox for yeetbullet2
+        {0,0,30,30} -- etc..
+    }
+    self.collider = Hitbox(self,unpack(hitboxes[yeet_id]))
+    
     -- Move the bullet in dir radians (0 = right, pi = left, clockwise rotation)
     self.physics.direction = dir
     -- Speed the bullet moves (pixels per frame at 30FPS)
