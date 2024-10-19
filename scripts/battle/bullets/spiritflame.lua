b0f776a48bc25ff06c9c48e4c068fa862834eeb8
chicken-nugget1104 <160080970+chicken-nugget1104@users.noreply.github.com>
2024-10-19 09:29:27 -0400

    manual change
    fix

--- scripts/battle/bullets/spiritflame.lua
+++ scripts/battle/bullets/spiritflame.lua
@@ -3,6 +3,7 @@ local SpiritFlame, super = Class(Bullet)
 function SpiritFlame:init(x, y)
     super.init(self, x, y, "bullets/flame") -- only texture change...
     self:setScale(1)
+    self.destroy_on_hit = true
 end
 
 return SpiritFlame
