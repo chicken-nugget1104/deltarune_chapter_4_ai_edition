955d11e1b81ba7870ce22fcfcc696502f9f66285
TFLTV <105890618+TFLTV@users.noreply.github.com>
2024-10-19 21:05:14 -0500

    Hmmmm

--- scripts/battle/waves/yeetwavother.lua
+++ scripts/battle/waves/yeetwavother.lua
@@ -8,10 +8,10 @@ function Basic2:onStart()
     local bullets = {}
     
     self.timer:every(1/3, function()
-        local x = SCREEN_WIDTH + 20
+        local x = SCREEN_WIDTH + 32
         local y = Utils.random(arena:getTop(), arena:getBottom())
         
-        local bullet = self:spawnBullet("yeet", x + 12 - Utils.random(1, 2), y - Utils.random(1, 2) + Utils.random(2, 3), math.rad(179), Utils.random(2, 10))
+        local bullet = self:spawnBullet("yeet", x - Utils.random(1, 2), y - Utils.random(1, 2) + Utils.random(2, 3), math.rad(179), Utils.random(2, 10))
         table.insert(bullets, bullet)
         
         bullet.remove_offscreen = false
