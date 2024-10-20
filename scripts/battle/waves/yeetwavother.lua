f4acb5b76eb8fc71d2e26992b854ac97350e0342
jmxamongusmodder <160080970+chicken-nugget1104@users.noreply.github.com>
2024-10-19 22:13:32 -0400

    Update yeetwavother.lua

--- scripts/battle/waves/yeetwavother.lua
+++ scripts/battle/waves/yeetwavother.lua
@@ -11,7 +11,7 @@ function Basic2:onStart()
         local x = SCREEN_WIDTH + 32
         local y = Utils.random(arena:getTop(), arena:getBottom())
         
-        local bullet = self:spawnBullet("yeet", x - Utils.random(1, 2), y - Utils.random(1, 2) + Utils.random(2, 3), math.rad(179), Utils.random(2, 10))
+        local bullet = self:spawnBullet("yeet", x - Utils.random(1, 2), y - Utils.random(1, 2) + Utils.random(2, 4), math.rad(179), Utils.random(2, 10))
         table.insert(bullets, bullet)
         
         bullet.remove_offscreen = false
