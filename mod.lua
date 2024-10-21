f8b4b87a99bd819cf1836111cbd33fb0f4778573
jmxamongusmodder <160080970+chicken-nugget1104@users.noreply.github.com>
2024-10-21 17:27:46 -0400

    fun test
    didnt work

--- mod.lua
+++ mod.lua
@@ -1,3 +1,9 @@
 function Mod:init()
     print("Loaded "..self.info.name.."!")
 end
+
+function Mod:onLoad(newsave) 
+    if not newsave then 
+	Game:setFlag("fun",Utils.random(1,100,1)) 
+    end 
+end
